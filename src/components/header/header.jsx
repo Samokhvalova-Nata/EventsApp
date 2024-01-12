d3a2cdfec900b6100abe34dff8c4f588ecf2d887
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-01-12 18:09:15 +0200

    Add styles for Home and About us pages

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
     return (
         <header>
-            <nav>
-                <img />
-                <Link href='/'>Home</Link>
-                <Link href='/events'>Events</Link>
-                <Link href='/about-us'>About Us</Link>
-            </nav>
+            <div className="header-container">
+            <div className="topNav">
+                <Link href='/'>
+                    <Image src={'/images/logo_black.png'} width={50} height={50} alt="logo"/>
+                </Link>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link href='/events'>Events</Link>
+                        </li>
+                        <li>
+                            <Link href='/about-us'>About Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            </div>
         </header>
     );
 };
--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -3,15 +3,18 @@ import Link from "next/link"
 
 export const HomePage = ({data}) => {
     return (
-        <main>
+        <div className="home-body">
 
         {data?.map(ev => 
-            <Link href={`/events/${ev.id}`} key={ev.id} passHref>
-                <Image src={ev.image} alt={ev.title} width={300} height={300}/>
-                <h2>{ev.title}</h2>
-                <p>{ev.description}</p>
-            </Link>)}
+            <Link className="card" href={`/events/${ev.id}`} key={ev.id} passHref>
+                <Image src={ev.image} alt={ev.title} width={600} height={400}/>
+                <div className="content">
+                    <h2>{ev.title}</h2>
+                    <p>{ev.description}</p>
+                </div>
+            </Link>
+            )}
 
-        </main>
+        </div>
     )
 }
