2dc1e97908378593cb1dd38d61e7b16528dc59d1
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-01-15 18:16:11 +0200

    Add email input validation

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 
 function buildPath() {
-    return path.join(process.cwd, 'data', 'data.json')
+    return path.join(process.cwd(), 'data', 'data.json')
 };
 
 function extractData(filePath) {
@@ -17,10 +17,42 @@ export default function handler (req, res) {
     const filePath = buildPath();
     const {events_categories, allEvents} = extractData(filePath);
 
+    if (!allEvents) {
+        return res.status(404).json({
+            status: 404,
+            message: 'Evants data not found'
+        })
+    }
+
     if (method === "POST") {
         const { email, eventId } = req.body;
+
+        if (!email || !email.includes('@')) {
+            res.status(422).json({
+                message: 'Invalid email address'
+            });
+            return;
+        }
+
+        const newAllEvents = allEvents.map( event => {
+            if (event.id === eventId) {
+                if (event.emails_registered.includes(email)) {
+                    res.status(409).json({
+                        message: 'This email has already been registered'
+                    });
+                    return event;
+                }
+                return {
+                    ...event, emails_registered: [ ...event.emails_registered, email]
+                }
+            }
+            return event;
+        });
+
+        fs.writeFileSync(filePath, JSON.stringify({events_categories, allEvents: newAllEvents}))
+
         res.status(200).json({
-            message: `You has been registered successfully with the email: ${email} ${eventId}`,
+            message: `You has been registered successfully with the email: ${email} for the event ${eventId}`,
         })
     }
 
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,15 +1,23 @@
-import React, { useRef } from 'react';
-import Image from 'next/image';
+import React, { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+
 
 export const SingleEvent = ({ data }) => {
     const inputEmail = useRef();
     const router = useRouter();
+    const [message, setMessage] = useState('');
 
     const onSubmit = async (e) => {
         e.preventDefault();
         const emailValue = inputEmail.current.value;
         const eventId = router?.query.id;
+
+        const validRegex =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (!emailValue.match(validRegex)) {
+            setMessage('Please introduse a correct email address')
+        }
 
         try {
             const response = await fetch('/api/email-registration', {
@@ -21,8 +29,10 @@ export const SingleEvent = ({ data }) => {
             });
             if(!response.ok) throw new Error(`Error: ${response.status}`)
             const data = await response.json();
+            setMessage(data.message);
+            inputEmail.current.value = '';
 
-            console.log('POST', data);
+            // console.log('POST', data);
 
         } catch (error) {
             console.log('error', error)
@@ -31,10 +41,10 @@ export const SingleEvent = ({ data }) => {
 
     return (
         <div className='single-event'>
-            <Image src={data.image} alt={data.title} width={1000} height={500}/>
+            <Image src={data.image} alt={data.title} width={1000} height={500} priority/>
             <h1>{data.title}</h1>
             <p>{data.description}</p>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit} className="email_registration">
                 <label>Get registered for this event</label>
                 <input 
                     ref={inputEmail} 
@@ -43,6 +53,7 @@ export const SingleEvent = ({ data }) => {
                     placeholder="Please insert your email here"/>
                 <button type="submit">Submit</button>
             </form>
+            <p>{message}</p>
         </div>
     );
 };
